422bddbf1fae624ecfe3fb1039deddb26f4ad0a2
elo <EloOfTheWired@proton.me>
2026-08-24 00:24:46 -0700

    premake file fix

--- premake5.lua
+++ premake5.lua
@@ -7,17 +7,16 @@ project "Secret-Output-Helper"
 
   targetdir "build/lib"
 
-
   filter "system:windows"
     systemversion "latest"
     defines { "PLATFORM_WINDOWS" }
-    files { "win64/CLIParser/**.cpp", "win64/common/**.hpp" }
-    removefiles { "win64/CLIParser/**LNX.cpp", "win64/common/**LNX.hpp" }
+    files { "outputHelper/**.cpp", "outputHelper/**.hpp" }
+    removefiles { "outputHelper/**LNX.cpp", "outputHelperLNX.hpp" }
 
   filter "system:linux"
     defines { "PLATFORM_LINUX" }
-    files { "linux/CLIParser/**.cpp", "linux/common/**.hpp" }
-    removefiles { "win64/CLIParser/**WIN.cpp", "win64/common/**WIN.hpp" }
+    files { "outputHelper/**.cpp", "outputHelper/**.hpp" }
+    removefiles { "outputHelper/**WIN.cpp", "outputHelper/**WIN.hpp" }
 
   filter "configurations:debug"
     defines "DEBUG"
